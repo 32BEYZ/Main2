12c06584c77845b12ca0983f87263cc5ae5d7d07
ABİDE BEYZA BAYRAM <abidebeyzabayram@icloud.com>
2022-09-18 12:48:12 +0300

    sayılarda asal sayı olma durumu
    asal sayı olma durumları incelendi

--- src/Home2.java
+++ src/Home2.java
@@ -5,6 +5,37 @@ public class Home2 {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello world2");
+		int number=852587;
+		int remainder=number%2;
+		System.out.println(remainder);
+		
+		
+		boolean isPrime=true;
+		
+		
+			
+for (int i=2; i<number;i++) {
+	if (number   % i ==0) {
+		isPrime =false;
+		
+				
+		
+	}
+	
+	
+	
+	
+}
+	
+	if(isPrime) {	
+		
+		System.out.println("sayı asaldır");
+	} else {
+		System.out.println("sayı asal değildir");
+	}
+		
+
+		
 	
 	
 		
